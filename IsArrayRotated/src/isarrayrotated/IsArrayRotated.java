4c12a36aca81153fa31910085c995db6d41168d1
zhadjah9559 <zhadjah@gmail.com>
2018-09-03 21:24:00 -0400

    Merge origin/master

--- IsArrayRotated/src/isarrayrotated/IsArrayRotated.java
+++ IsArrayRotated/src/isarrayrotated/IsArrayRotated.java
@@ -3,17 +3,24 @@ Zachary Hadjah                      February 9th, 2018
 Junior Year                         Side Project
 
 Description:
-This program will take two arrays, one original and one array that will be compared 
+This program will create two arrays, one original and one array that will be compared 
 to the original. The isRotated function will check to see if the lengths are both 
 the same size for the elements. It will then check whether the array has been
 rotated by checking the elements of the original from the first element to the 
 last, while the other array will be checked from the last element up until the 
-first.
+first. 
+Next, this program will tell you to enter an array of single digit integers. The 
+rotateArray function will then rotate the numbers the user enters. 
+For example:
+
+    User input: 1 2 3 4 5
+    Function output: 5 4 3 2 1
 ==============================================================================*/
 package isarrayrotated;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class IsArrayRotated 
 {
@@ -41,29 +48,61 @@ public class IsArrayRotated
         }
         
         System.out.println("\n Watch what happens when the original array is compared to an array"
-                         + "with contents \n [9,8,7,6,5,4,3,2,1,0]");
+                         + " with contents \n [9,8,7,6,5,4,3,2,1,0]");
         
         rotation = isRotated(original, rotatedArray);
-        if(rotation == true)
+        if(rotation)
         {
             System.out.println("The original array has been rotated!");
         }else{
             System.out.println("The original array has not been rotated");
         }
         
-        //user will enter array and will get rotated
-        int[] userArray = new int[9];
+        ArrayList<Integer> userArray = new ArrayList<Integer>(9);
         
-        System.out.println("This program can also rotate arrays.\n" +
-                           "Enter 9 single digit integers separated by spaces");
-        for(int i = 0; i<userArray.length; i++)
+        System.out.println("This program can also rotate arrays that contain "
+                         + "single digit integers.\n Enter 9 single digit "
+                         + "integers separated by spaces");
+        
+        /*
+        userArray.add(scan.nextInt());
+        for(int i = 0; i<userArray.size(); i++)
         {
-            userArray[i] = scan.nextInt();
+            //*****problem
+            if(scan.next(). )
+                break;
+            else         
+                userArray.add(scan.nextInt());
+        }*/
+        
+        
+        //reads input as string, will check to see if element in array is empty or not
+        String input = scan.nextLine();
+        String[] numbers = input.split(" ");
+        for(String number:numbers)
+        {
+            try
+            {
+                if(number.isEmpty())
+                continue;
+                userArray.add(Integer.parseInt(number));
+            }   
+            catch(Exception e)
+            {
+                System.out.println("Invalid input. Please enter single digit "
+                                 + "integers");
+            }
         }
         
-        System.out.println("The array you entered is: " + Arrays.toString(userArray) +"\n"+
-                           "When your array is rotated, it looks like this: \n" + 
-                           Arrays.toString(rotateArray(userArray)));
+        
+        
+        System.out.println("The array you entered is: " + userArray.toString() +"\n");
+        
+        rotateArray(userArray);
+        
+        System.out.println("When your array is rotated, it looks like this: \n" + 
+                           userArray.toString());
+                           
     }
     
     public static boolean isRotated(int[] original, int[] tempArray)
@@ -89,19 +128,20 @@ public class IsArrayRotated
     }    
     
     //Will rotate the array that the user entered 
-    public static int[] rotateArray(int[] userArray)
+    public static ArrayList<Integer> rotateArray(ArrayList<Integer> userArray)
     {    
         int replace = 0;
         int inc = 1;
+        int indexVariable = 0;
         
         //if number of elements equals an even number
-        if(userArray.length % 2 == 0)
+        if(userArray.size() % 2 == 0)
         {
-            for(int i = 0; i < (userArray.length /2);i++)
+            for(int i = 0; i < (userArray.size()/2);i++)
             {
-                replace = userArray[i];
-                userArray[i] = userArray.length - inc;
-                userArray[userArray.length - inc] = replace;
+                replace = userArray.get(i);                             
+                userArray.set(userArray.get(i),userArray.size() - inc );
+                userArray.set(userArray.size() - inc, replace);
                 inc++;
             }
         }
@@ -109,16 +149,16 @@ public class IsArrayRotated
         //if number of elements equals an odd number
         else
         {
-            for (int i = 0; i <(userArray.length/2) ; i++) 
+            for (int i = 0; i <(userArray.size()/2) ; i++) 
             {
-                replace=userArray[i];
-                userArray[i]=userArray[userArray.length-inc];
-                userArray[userArray.length-inc]=replace;
+                replace = userArray.get(i);                             
+                userArray.set(userArray.get(i),userArray.size() - inc );
+                userArray.set(userArray.size() - inc, replace);
                 inc++;
             }
         }
+        
         return userArray;
-    }
-    
-   
+        
+    }  
 }
